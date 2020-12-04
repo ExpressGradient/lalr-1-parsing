157e98a314fd4e5522e335f612f77eb2d371e7e2
Praneeth <johndoeknown@protonmail.com>
2020-12-04 13:46:32 +0530

    added DFA

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def fancy_print(some_array):
 # Production Rule Format
 class ProdRule:
     def __init__(self, derive_symbol, derivations, look_ahead=None):
-        self.derive_symbol = derive_symbol
-        self.derivations = derivations
-        self.look_ahead = [] if look_ahead is None else look_ahead
+        self.derive_symbol: str = derive_symbol
+        self.derivations: list = derivations
+        self.look_ahead: list or None = [] if look_ahead is None else look_ahead
 
     def __repr__(self):
         return f"{self.derive_symbol} -> {self.derivations}, look ahead: {self.look_ahead}"
@@ -22,11 +22,11 @@ class State:
     state_num = 0
 
     def __init__(self, prod_rules):
-        self.state_num = f"I{State.state_num}"
-        self.prod_rules = prod_rules
-        self.from_transitions = []
-        self.to_transitions = []
-        self.accept_state = False
+        self.state_num: str = f"I{State.state_num}"
+        self.prod_rules: list = prod_rules
+        self.from_transitions: list = []
+        self.to_transitions: list = []
+        self.accept_state: bool = False
 
         State.state_num += 1
 
@@ -66,9 +66,9 @@ def apply_points(prod_rules, find_derive_symbol, track):
             track.append(find_derive_symbol)
             for i in range(len(rule.derivations)):
                 rule.derivations[i] = f".{rule.derivations[i]}"
-                dot_index = rule.derivations[i].index(".")
-                if dot_index + 1 != len(rule.derivations[i]) and rule.derivations[i][dot_index + 1].isupper():
-                    apply_points(prod_rules, rule.derivations[i][dot_index + 1], track)
+                dot_idx = rule.derivations[i].index(".")
+                if dot_idx + 1 != len(rule.derivations[i]) and rule.derivations[i][dot_idx + 1].isupper():
+                    apply_points(prod_rules, rule.derivations[i][dot_idx + 1], track)
 
     return prod_rules
 
@@ -79,12 +79,12 @@ def extract_look_ahead(prod_rules):
         if prod_rules.index(rule) > 0:
             prev_rule = prod_rules[prod_rules.index(rule) - 1]
             for prev_rule_derivation in prev_rule.derivations:
-                dot_index = prev_rule_derivation.index(".")
+                dot_idx = prev_rule_derivation.index(".")
                 if len(prev_rule_derivation) == 2:
                     rule.look_ahead.append(prev_rule.look_ahead[prev_rule.derivations.index(prev_rule_derivation)])
-                elif len(prev_rule_derivation) > 2 and prev_rule_derivation[dot_index + 2].isupper():
+                elif len(prev_rule_derivation) > 2 and prev_rule_derivation[dot_idx + 2].isupper():
                     for rule_j in prod_rules:
-                        if rule_j.derive_symbol == prev_rule_derivation[dot_index + 2]:
+                        if rule_j.derive_symbol == prev_rule_derivation[dot_idx + 2]:
                             for rule_j_derivation in rule_j.derivations:
                                 if rule_j_derivation[1].islower():
                                     rule.look_ahead.append(rule_j_derivation[1])
@@ -92,23 +92,17 @@ def extract_look_ahead(prod_rules):
     return prod_rules
 
 
-def dfa_apply_points(state_machine):
-    pass
-
-
-# For constructing DFA from production_rules
-def construct_dfa(state_machine):
-    state = state_machine[0]
-    for rule in state.prod_rules:
-        for derivation in rule.derivations:
-            dot_index = derivation.index(".")
-            derivation_index = rule.derivations.index(derivation)
-            if dot_index + 1 != len(derivation):
-                swap_string = derivation[dot_index: dot_index + 2]
-                swap_string = swap_string[::-1]
-                rule.derivations[derivation_index] = derivation[:dot_index] + swap_string + derivation[dot_index + 3:]
-            state_machine.append(
-                State(ProdRule(rule.derive_symbol, rule.derivations[derivation_index], rule.look_ahead)))
+# For constructing the DFA
+def construct_dfa(state_machine, current_state_idx):
+    current_state = state_machine[current_state_idx]
+    temp_rules = current_state.prod_rules
+    for (rule_idx, rule) in enumerate(temp_rules):
+        for (derivation_idx, derivation) in enumerate(rule.derivations):
+            dot_idx = derivation.index(".")
+            if dot_idx + 1 != len(derivation):
+                swap_str = derivation[dot_idx: dot_idx + 2]
+                swap_str_rev = swap_str[::-1]
+                temp_rules[rule_idx].derivations[derivation_idx] = derivation.replace(swap_str, swap_str_rev)
 
 
 with open("input.txt", "r") as input_file:
@@ -123,6 +117,6 @@ with open("input.txt", "r") as input_file:
     extract_look_ahead(prod_rules)
 
     state_machine = [State(prod_rules)]
-    construct_dfa(state_machine)
+    construct_dfa(state_machine, 0)
 
     fancy_print(state_machine)
